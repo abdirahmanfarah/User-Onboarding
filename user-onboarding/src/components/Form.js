8ca6c5e1eb16139d45ceae7c30845fe5189a6b94
abdirahmanfarah <abdirahman.z.farah@gmail.com>
2019-09-19 15:31:37 -0700

    Finished with posting my data, now to the display and styling

--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -1,17 +1,27 @@
 import React from "react";
-import { withFormik, Form, Field } from 'formik'
+import { withFormik, Form, Field } from 'formik';
+import * as yup from 'yup';
+import axios from 'axios'
 
 
-const User = props => {
-  console.log(props);
+const User = ({errors, touched }) => {
+  // console.log(props);
 
   return (
     <Form>
-      <Field type="text" name="name" placeholder = "Name" />
-      <Field type="text" name="email" placeholder ="Email"/>
-      <Field type="password" name ="password" placeholder ="Password"/>
+      {touched.Name && errors.Name && <p>{errors.Name}</p>}
+      <Field type="text" name="Name" placeholder = "Name" />
+
+      {touched.Email && errors.Email && <p>{errors.Email}</p>}
+      <Field type="text" name="Email" placeholder ="Email"/>
+      
+      {touched.Password && errors.Password && <p>{errors.Password}</p>}
+      <Field type="password" name ="Password" placeholder ="Password"/>
+
       <Field component="textarea" name = "note" placeholder ="Notes" />
+      <span>Terms and Services</span>
       <Field className ="check" type ="checkbox" name="terms" />
+      {touched.terms && errors.terms && <p>{errors.terms}</p>}
       <button type ="submit"> Submit</button>
 
     </Form>
@@ -21,14 +31,29 @@ const User = props => {
 export default withFormik({
   mapPropsToValues:(values) => {
     return {
-      name: values.name || "",
-      email: values.email || "",
-      password: values.password || "",
+      Name: values.Name || "",
+      Email: values.Email || "",
+      Password: values.Password || "",
       terms: values.terms || false,
       note:values.note
     }
   },
+  validationSchema: yup.object().shape({
+      Name: yup.string().required(),
+      Email: yup.string().required(),
+      Password: yup.string().required(),
+      terms: yup.boolean().oneOf([true], "You must agree to the Terms of Service")
+
+  }),
   handleSubmit:(values) => {
-    console.log(values)
+    // console.log(values)
+    axios 
+        .post("https://reqres.in/api/users", values)
+          .then(res => {
+            console.log(res)
+          })
+          .catch((err) => {
+            console.log('Error:', err)
+          })
   }
 })(User);
