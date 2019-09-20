00d2cdac535d8851ca9998b34dc0e2534ad596f8
abdirahmanfarah <abdirahman.z.farah@gmail.com>
2019-09-19 17:53:14 -0700

    reached MVP

--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { withFormik, Form, Field } from 'formik';
 import * as yup from 'yup';
 import axios from 'axios'
 
 
-const User = ({errors, touched }) => {
-  // console.log(props);
+const User = ({errors, touched, status}) => {
+  // console.log(status);
+
+  const initialMember = { name: "", email: "", password: "", terms: false, notes: ""};
+  const [newUser, setNewUser] = useState([]);
+
+  useEffect(() => {
+    if(status){
+
+      setNewUser([...newUser, status])
+    }
+  },[status])
 
   return (
     <Form>
@@ -24,6 +34,14 @@ const User = ({errors, touched }) => {
       {touched.terms && errors.terms && <p>{errors.terms}</p>}
       <button type ="submit"> Submit</button>
 
+      {newUser.map(user =>(
+        <ul className ="list" key={user.id} >
+
+          <li > Name: {user.Name}</li>,
+          <li> Email: {user.Email}</li>
+        </ul>
+      ))}
+           
     </Form>
   )
 }
@@ -45,15 +63,15 @@ export default withFormik({
       terms: yup.boolean().oneOf([true], "You must agree to the Terms of Service")
 
   }),
-  handleSubmit:(values) => {
+  handleSubmit:(values, { setStatus}) => {
     // console.log(values)
     axios 
         .post("https://reqres.in/api/users", values)
           .then(res => {
-            console.log(res)
+            setStatus(res.data)
           })
           .catch((err) => {
             console.log('Error:', err)
           })
-  }
+        }
 })(User);
